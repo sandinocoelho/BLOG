d2522accef35f2b9ff6bf5302d8f0b813716988c
Maymisson <maymisson@gmail.com>
2019-10-14 20:20:33 -0300

    Inserção de function e edição de nomenclatura

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:false}))
 
 app.get('/', (req, res) => {
-    res.send('Funcionando...')
+    res.send('Working...')
 })
 
 const rotas = require('./rotas')
--- rotas/postagens.js
+++ rotas/postagens.js
@@ -6,20 +6,20 @@ router.get('/postagens', (req, res) => {
     
     ibmdb.open("DATABASE=BLUDB;HOSTNAME=dashdb-txn-sbox-yp-dal09-03.services.dal.bluemix.net;UID=mhh09699;PWD=z90km068zw@8z21p;PORT=50000;PROTOCOL=TCPIP", (err, conn) =>{
         if (err){
-            return console.log(err)
+            return console.log({erro: err, message : 'connection failed' })
         }
 
-        conn.query('select * from MHH09699.POSTAGENS', (err, data) =>{
+        conn.query('select * from MHH09699.POSTAGENS', function (err, data) {
             if (err){
-                console.log({erro: err})
+                console.log({erro: err, message : 'Query select failed' })
             }else{
                 console.log(data)
                 res.send(data)
             }
         })
 
-        conn.close(() =>{
-            console.log('conexão encerrada')
+        conn.close(function() {
+            console.log('Connection terminated')
         })
 
     })
@@ -28,17 +28,18 @@ router.get('/postagens', (req, res) => {
 router.get('/postagens/new', (req, res) =>{
     ibmdb.open("DATABASE=BLUDB;HOSTNAME=dashdb-txn-sbox-yp-dal09-03.services.dal.bluemix.net;UID=mhh09699;PWD=z90km068zw@8z21p;PORT=50000;PROTOCOL=TCPIP", (err, conn) =>{
         if(err){
-            return console.log({erro: err, mensagem: 'connection failed'})
+            return console.log({erro: err, message: 'connection failed'})
         }
-        conn.query(`INSERT INTO MHH09699.POSTAGENS (TITULO,CONTEUDO,AUTOR) values ('${req.query.titulo}','${req.query.conteudo}','${req.query.autor}')`, (err, data) =>{
+        conn.query(`INSERT INTO MHH09699.POSTAGENS (TITULO,CONTEUDO,AUTOR) values ('${req.query.titulo}','${req.query.conteudo}','${req.query.autor}')`, 
+        function (err, data){
             if(err) {
-                console.log({erro: err, mensagem: 'Query insert filed'})
+                console.log({erro: err, message: 'Query insert filed'})
             }else {
                 console.log(data)
                 res.send('Post successful')
             }
-            conn.close(() =>{
-                console.log('conexão encerrada')
+            conn.close(function(){
+                console.log('Connection terminated')
             })
         })
     }) 
