34b4f0facd634f662bd6a7da380333524685f3f3
Maymisson <maymisson@gmail.com>
2019-10-14 10:43:49 -0300

    Conexão BackEnd com o Ibm_DB, estruturação completa.

--- rotas/postagens.js
+++ rotas/postagens.js
@@ -1,12 +1,47 @@
 const express = require('express')
 const router = express.Router()
+const ibmdb = require('ibm_db')
 
 router.get('/postagens', (req, res) => {
-    res.json({
-        titulo: 'Primeira postagem',
-        conteudo: 'Uma nova publicação',
-        autor: 'Maymisson Magno'
+    
+    ibmdb.open("DATABASE=BLUDB;HOSTNAME=dashdb-txn-sbox-yp-dal09-03.services.dal.bluemix.net;UID=mhh09699;PWD=z90km068zw@8z21p;PORT=50000;PROTOCOL=TCPIP", (err, conn) =>{
+        if (err){
+            return console.log(err)
+        }
+
+        conn.query('select * from MHH09699.POSTAGENS', (err, data) =>{
+            if (err){
+                console.log({erro: err})
+            }else{
+                console.log(data)
+                res.send(data)
+            }
+        })
+
+        conn.close(() =>{
+            console.log('conexão encerrada')
+        })
+
     })
+})
+
+router.get('/postagens/new', (req, res) =>{
+    ibmdb.open("DATABASE=BLUDB;HOSTNAME=dashdb-txn-sbox-yp-dal09-03.services.dal.bluemix.net;UID=mhh09699;PWD=z90km068zw@8z21p;PORT=50000;PROTOCOL=TCPIP", (err, conn) =>{
+        if(err){
+            return console.log({erro: err, mensagem: 'connection failed'})
+        }
+        conn.query(`INSERT INTO MHH09699.POSTAGENS (TITULO,CONTEUDO,AUTOR) values ('${req.query.titulo}','${req.query.conteudo}','${req.query.autor}')`, (err, data) =>{
+            if(err) {
+                console.log({erro: err, mensagem: 'Query insert filed'})
+            }else {
+                console.log(data)
+                res.send('Post successful')
+            }
+            conn.close(() =>{
+                console.log('conexão encerrada')
+            })
+        })
+    }) 
 })
 
 module.exports = router
